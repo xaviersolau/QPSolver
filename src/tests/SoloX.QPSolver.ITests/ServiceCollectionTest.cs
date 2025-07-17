67d98459d04ce33ea773728c79dbfe613d604bfd
Xavier Solau <xaviersolau@yahoo.fr>
2025-07-17 23:29:20 +0200

    Add SoloX.QPSolver.ITests

---
+++ src/tests/SoloX.QPSolver.ITests/ServiceCollectionTest.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ServiceCollectionTest.cs" company="Xavier Solau">
+// Copyright © 2025 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SoloX.QPSolver.ITests
+{
+    public class ServiceCollectionTest
+    {
+        [Fact]
+        public void ItShouldBeAvailableThroughDependencyInjection()
+        {
+            var services = new ServiceCollection();
+
+            services.AddQuadraticProgrammingEngine();
+
+            using var provider = services.BuildServiceProvider();
+
+            var qpEngine = provider.GetRequiredService<IQPEngine>();
+
+            qpEngine.Should().NotBeNull();
+        }
+    }
+}
